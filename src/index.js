158580206cf78be3308044b3b4c7e37f2120ee26
Davis Ulrich <60760602+davisulrich@users.noreply.github.com>
2022-02-28 19:48:15 -0800

    added paddle

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import "./styles.css";
+import Paddle from "/src/paddle";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+let canvas = document.getElementById("gameScreen");
+
+let context = canvas.getContext("2d");
+
+context.clearRect(0, 0, 800, 600);
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+/*
+context.fillStyle = "#f00";
+context.fillRect(20, 20, 100, 100);
+
+context.fillStyle = "#00f";
+context.fillRect(680, 480, 100, 100);
+*/
+
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+
+paddle.draw(context);
